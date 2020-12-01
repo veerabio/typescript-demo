00fe6ececa7772f1466620004122524699858185
veera <veera_cheedepudi-reddy@bio-rad.com>
2020-11-30 17:09:04 -0800

    fix declaration

--- src/2-basic-types/index.ts
+++ src/2-basic-types/index.ts
@@ -14,7 +14,7 @@ let big: bigint = 100n;
 
 // array
 let list: number[] = [1, 2, 3];
-let list: Array<number> = [1, 2, 3];
+let list1: Array<number> = [1, 2, 3];
 
 // tuple
 let x: [string, number];
