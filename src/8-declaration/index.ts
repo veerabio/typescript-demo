4b94010641f5301f053005f4891e49ac2b8ae844
veera <veera_cheedepudi-reddy@bio-rad.com>
2020-12-01 12:08:37 -0800

    add underscore

--- src/8-declaration/index.ts
+++ src/8-declaration/index.ts
@@ -1,5 +1,9 @@
-import { random } from 'lodash';
+import { random as randomLoadash } from 'lodash';
+import { random as randomUnder } from 'underscore';
 
-const r1 = random(0, 5);
+const r1 = randomLoadash(0, 5);
+
+const r2 = randomUnder(0, 1);
 
 console.log(r1);
+console.log(r2);
