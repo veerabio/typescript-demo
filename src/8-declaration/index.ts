e4b1ab46fa64f8e9e9e35a2dd63bbad58df6ddb9
veera <veera_cheedepudi-reddy@bio-rad.com>
2020-12-01 12:00:02 -0800

    add custom typing

--- src/8-declaration/index.ts
+++ src/8-declaration/index.ts
@@ -1,5 +1,5 @@
 import { random } from 'lodash';
 
-const r1 = random(0, 1);
+const r1 = random(0, 5);
 
 console.log(r1);
---
+++ src/types/lodash/index.d.ts
@@ -0,0 +1,3 @@
+declare module 'lodash' {
+    declare var random : (x: number, y: number) => number;
+}
